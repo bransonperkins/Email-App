4f2b2ac5e2fe4bbbea48cd80abc92d4c78e30fce
Branson Perkins <bransonperkins@gmail.com>
2020-05-22 10:20:25 -0400

    completed email app following along with Udemy tutorial

---
+++ src/emailapp/EmailApp.java
@@ -0,0 +1,10 @@
+package emailapp;
+
+public class EmailApp {
+    public static void main(String[] args) {
+
+        Email email1 = new Email("Branson", "Perkins");
+        email1.showInfo();
+
+    }
+}
